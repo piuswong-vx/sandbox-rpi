71c4d20e53472aec13f69ccfb9f7adcfac2b6553
Pius Wong <pius@voting.works>
2023-11-08 15:51:42 -0600

    Fix test code for controlling GPIO output

--- test-led.py
+++ test-led.py
@@ -7,9 +7,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(18,GPIO.OUT)
-print "LED on"
+# check pinout.xyz for pin diagram
+print("LED on -- GPIO18, physical pin 12; grounded at physical pin 6")
 GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
+time.sleep(10)
 
-print "LED off"
+print("LED off")
 GPIO.output(18,GPIO.LOW)
