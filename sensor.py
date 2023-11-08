cc31293d07a85623e14856d81d7db4bedc26a4e6
Pius Wong <pius@voting.works>
2023-11-08 16:11:52 -0600

    Object detection leads to GPIO output change

--- sensor.py
+++ sensor.py
@@ -1,8 +1,14 @@
 import qwiic
 import qwiic_bme280
 import qwiic_vl53l1x
+import RPi.GPIO as GPIO
 import time
 import sys
+
+# Set up output signal
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
 
 # For checking devices available:
 # results = qwiic.list_devices()
@@ -10,11 +16,15 @@ import sys
 
 print("Running sensor.py...")
 
-def runExample():
+def runSensors():
 
     print("\nVxWorks testing \n")
+    
+    # initialize sensors and data
     sensorAtmospheric = qwiic_bme280.QwiicBme280()
     sensorDist1 = qwiic_vl53l1x.QwiicVL53L1X()
+    distance1 = 999
+    distance1past = 999
 
     if sensorAtmospheric.is_connected() == False:
         print("The Qwiic BME280 (atmospheric) device isn't connected to the system.", 
@@ -43,16 +53,23 @@ def runExample():
 
         print("Temp (F):\t%.2f" % sensorAtmospheric.temperature_fahrenheit)
         
-#       distance code
+        # distance code
         sensorDist1.start_ranging()
         distance1 = sensorDist1.get_distance()
         time.sleep(.005)
         sensorDist1.stop_ranging()
         print("Dist 1 (mm):\t%.1f" % distance1)
-        
+        if (distance1 < 200):
+            print("** Distance sensor detects object! **")
+            GPIO.output(18,GPIO.HIGH)
+        else:
+            GPIO.output(18,GPIO.LOW)
+            if (distance1past < 200):
+                print("* Object no longer detected. *")
+        print("")                
+        distance1past = distance1
 
-        print("")
-
+        # wait
         time.sleep(1)
 
-runExample()
+runSensors()
