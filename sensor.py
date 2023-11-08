296f673b781cbee3251c0b17c27f6901c61fe952
Pius Wong <pius@voting.works>
2023-11-08 13:16:18 -0600

    Read both distance and atmospheric sensor data

--- sensor.py
+++ sensor.py
@@ -1,4 +1,5 @@
 import qwiic_bme280
+import qwiic_vl53l1x
 import time
 import sys
 
@@ -6,28 +7,47 @@ print("Running sensor.py...")
 
 def runExample():
 
-    print("\nSparkFun BME280 Sensor + Raspberry Pi Example \n")
-    mySensor = qwiic_bme280.QwiicBme280()
+    print("\nVxWorks testing \n")
+    sensorAtmospheric = qwiic_bme280.QwiicBme280()
+    sensorDist1 = qwiic_vl53l1x.QwiicVL53L1X()
 
-    if mySensor.is_connected() == False:
-        print("The Qwiic BME280 device isn't connected to the system.", 
+    if sensorAtmospheric.is_connected() == False:
+        print("The Qwiic BME280 (atmospheric) device isn't connected to the system.", 
+            file=sys.stderr)
+        return
+    else:
+        print("The Qwiic BME280 (atmospheric) device is online!")
+        
+    if (sensorDist1.sensor_init() == None):
+        print("The Qwiic LV531X (IR distance) is online!")
+    else:
+        print("The Qwiic LV531X (IR distance) device isn't connected to the system.", 
             file=sys.stderr)
         return
 
-    mySensor.begin()
+    print("\n======\n")
+    
+    sensorAtmospheric.begin()
 
     while True:
-        print("Humidity:\t%.3f" % mySensor.humidity)
+        print("Humidity (RH):\t%.3f" % sensorAtmospheric.humidity)
 
-        print("Pressure:\t%.3f" % mySensor.pressure)    
+        print("Pressure (kPa):\t%.3f" % sensorAtmospheric.pressure)    
 
-        print("Altitude:\t%.3f" % mySensor.altitude_feet)
+        print("Altitude (ft):\t%.3f" % sensorAtmospheric.altitude_feet)
 
-        print("Temperature:\t%.2f" % mySensor.temperature_fahrenheit)       
+        print("Temp (F):\t%.2f" % sensorAtmospheric.temperature_fahrenheit)
+        
+#       distance code
+        sensorDist1.start_ranging()
+        distance1 = sensorDist1.get_distance()
+        time.sleep(.005)
+        sensorDist1.stop_ranging()
+        print("Dist 1 (mm):\t%.1f" % distance1)
+        
 
         print("")
 
         time.sleep(1)
-
 
 runExample()
